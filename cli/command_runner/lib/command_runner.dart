071f98051dc9766a9a68889b4b1fafa98322d4e1
Kash <Kashyap.8@yahoo.com>
2026-09-18 15:38:29 -0700

    feat: ArgResults class added to argumrnts.dart

--- cli/command_runner/lib/command_runner.dart
+++ cli/command_runner/lib/command_runner.dart
@@ -1,8 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+
 library;
 
 export 'src/command_runner_base.dart';
+export 'src/arguments.dart';
 
-// TODO: Export any libraries intended for clients of this package.
