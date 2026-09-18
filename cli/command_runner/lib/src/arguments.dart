603607c9e4eb26b81a3159f460ec037a4a4190f0
Kash <Kashyap.8@yahoo.com>
2026-09-18 14:49:53 -0700

    feat: added arguments.dart for Option class

---
+++ cli/command_runner/lib/src/arguments.dart
@@ -0,0 +1,29 @@
+enum OptionType {flag, option}
+
+class Option {
+  Option(
+    this.name, {
+    required this.type,
+    this.help,
+    this.abbr,
+    this.defaultValue,
+    this.valueHelp,
+  });
+
+
+  final String name;
+  final OptionType type; 
+  final String? help; 
+  final String? abbr;
+  final Object? defaultValue; 
+  final String? valueHelp; 
+
+  String get usage {
+    if (abbr != null) {
+      return '-$abbr,--$name: $help';
+    }
+
+    return '--$name: $help'; 
+  }
+  
+}
